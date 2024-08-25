4e73b5711d2f43f9dd1b77cd5236f1453e63916c
Rahul Bali <rahulrdb18@gmail.com>
2024-08-25 12:26:43 +0530

    subit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,10 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, Inject, PLATFORM_ID } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { isPlatformBrowser } from '@angular/common';
 
 import { NgxGoogleAnalyticsModule } from '@hakimio/ngx-google-analytics';
+
+import bb, { line } from 'billboard.js';
 
 
 @Component({
@@ -14,6 +17,27 @@ import { NgxGoogleAnalyticsModule } from '@hakimio/ngx-google-analytics';
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
-export class AppComponent {
-  title = 'angular-starter';
+export class AppComponent implements OnInit {
+  title = 'graph-india';
+
+  constructor(@Inject(PLATFORM_ID) private platformId: Object) {}
+
+  ngOnInit(): void {
+    if (isPlatformBrowser(this.platformId)) {
+      this.generateChart();
+    }
+  }
+
+  generateChart(): void {
+    bb.generate({
+      data: {
+        columns: [
+          ["data1", 30, 200, 100, 400, 150, 250],
+          ["data2", 50, 20, 10, 40, 15, 25]
+        ],
+        type: line(), // for ESM specify as: line()
+      },
+      bindto: "#chart"
+    });
+  }
 }
